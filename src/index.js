9331c8c2e8c8e3057d23443466b35b7c8d2be010
Tang Yang <ty4081097@gmail.com>
2019-06-15 21:56:47 -0500

    modify code.

--- src/index.js
+++ src/index.js
@@ -27,9 +27,9 @@ const typeDefs = `
     id: ID! 
     firstName: String!
     lastName: String!
-    jobTitile: String!
+    jobTitle: String!
     departmentId: Float
-    managerId: 
+    managerId: Float
     }
 
 `
