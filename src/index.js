a92e14a1441a25c964e1f827ee479d6f66d9ea3e
Tang Yang <ty4081097@gmail.com>
2019-06-16 10:34:10 -0500

    made update to file.

--- src/index.js
+++ src/index.js
@@ -5,21 +5,45 @@ const people = [{
     firstName: 'Orval',
     lastName: 'Hauck',
     jobTitle: 'CEO',
-    departmentId: "2b9edccb-41fc-4fc5-b832-ac86a034a877"
+    
 },
 {
     id: 'd44390cd-b306-4e11-b7d5-a5e0e6fe1e3d',
     firstName: 'Asia',
     lastName: 'Streich',
     jobTitle: 'Dynamic Branding Orchestrator',
-    departmentId: 'aef293ee-8dcc-4d89-99cf-1b8f61bab07b',
-    managerId: '2798c35b-5b8f-4a5d-9858-0a818d48cbef'
-
+   
+    
 }]
+
+// const departments = [{
+//        "name": "Engineering", 
+//        "id": "920a774e-617a-4a5b-82ea-8205c18eef75" 
+//     },{ 
+//         "name": "Sales", 
+//         "id": "cfd90465-28fa-4b9a-be3e-ef2517e987e9" 
+//     },{
+//         "name": "Marketing", 
+//         "id": "252fc1e8-aead-45cc-9d7d-e6003897bbf9" 
+//     },{
+//         "name": "Operations", 
+//         "id": "e573dd1c-4cd4-451d-a844-a25210e91135" 
+//     },{ 
+//         "name": "Management", 
+//         "id": "2b9edccb-41fc-4fc5-b832-ac86a034a877" 
+//     },{ 
+//         "name": "Executive", 
+//         "id": "aef293ee-8dcc-4d89-99cf-1b8f61bab07b" 
+//     },{ 
+//         "name": "HR", 
+//         "id": "ddd31c01-a30d-4e72-8e8b-d710fcc4fb56" 
+    
+// }]
 
 const typeDefs = `
     type Query {
        people(query: String): [people!]!
+       
 
     }
 
@@ -28,10 +52,11 @@ const typeDefs = `
     firstName: String!
     lastName: String!
     jobTitle: String!
-    departmentId: Float
-    managerId: Float
+    
+    
     }
 
+    
 `
 
 // Resolvers
